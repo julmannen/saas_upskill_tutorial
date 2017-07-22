73cb61be458fe20fff2641b2bd5eb971c1013f3e
= <=>
2017-07-22 15:06:22 +0000

    created contacts_controller, view, route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     
     get 'about', to: 'pages#about'
+    
+    resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
